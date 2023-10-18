a3bcc99d7b30a54cca06aaca8920359b94b277b4
Tomasz Osuch <tomasz.osuch@gmail.com>
2023-10-18 16:41:35 +0200

    [REDDIT-CLONE] first commit JUnit test all repository

---
+++ src/test/java/pl/tomaszosuch/redditclone/repository/UserRepositoryTest.java
@@ -0,0 +1,22 @@
+package pl.tomaszosuch.redditclone.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testFindByUsername() {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
